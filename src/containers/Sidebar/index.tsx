8a15dc20d26235f1b79e099bfa5c589d02a7e787
Nichoas Ito <nicholas.ito94@gmail.com>
2023-08-31 14:52:26 -0300

    construindo a estrutura e a seção "sobre"

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Title from '../../components/Title'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/ito-0.png" />
+    <Title fontSize={20}>Nicholas Ito</Title>
+  </aside>
+)
+
+export default Sidebar
