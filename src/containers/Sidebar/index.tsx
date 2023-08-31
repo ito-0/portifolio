b1e81415fc98fb3f9596e48449572adae4274ad7
Nichoas Ito <nicholas.ito94@gmail.com>
2023-08-31 16:39:13 -0300

    styling sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Title from '../../components/Title'
+
+import { BotaoTema, Descricao, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/ito-0.png" />
-    <Title fontSize={20}>Nicholas Ito</Title>
+    <SidebarContainer>
+      <Avatar />
+      <Title fontSize={20}>Nicholas Ito</Title>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        ito-0
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro Front-End
+      </Descricao>
+      <BotaoTema>Trocar Tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
